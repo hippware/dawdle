1a014408af113a389f3d44a51da8c7d1f80a9e9c
Bernard Duggan <bernard@hippware.com>
2018-04-17 17:16:29 +1000

    Add more tests

--- test/procrastinator_test.exs
+++ test/procrastinator_test.exs
@@ -15,8 +15,39 @@ defmodule ProcrastinatorTest do
 
   test "send message with 1s delay" do
     Procrastinator.send(@payload, 1000)
-    refute_receive @payload, 800
+    refute_receive _, 800
     assert_receive @payload, 300
+  end
+
+  @intervals [1000, 5000, 10000]
+  test "send 10 messages with various delays" do
+    @intervals
+    |> Enum.map(&List.duplicate(&1, 10))
+    |> List.flatten()
+    |> Enum.shuffle()
+    |> Enum.each(&Procrastinator.send(&1, &1))
+
+    :timer.send_after(800, :none)
+    :timer.send_after(1200, :receive)
+    :timer.send_after(4800, :none)
+    :timer.send_after(5200, :receive)
+    :timer.send_after(9800, :none)
+    :timer.send_after(10200, :receive)
+
+    assert_receive :none, 1000
+    refute_receive _, 0
+    assert_receive :receive, 5000
+    Enum.each(1..10, fn _ -> assert_received 1000 end)
+
+    assert_receive :none, 4000
+    refute_receive _, 0
+    assert_receive :receive, 5000
+    Enum.each(1..10, fn _ -> assert_received 5000 end)
+
+    assert_receive :none, 5000
+    refute_receive _, 0
+    assert_receive :receive, 5000
+    Enum.each(1..10, fn _ -> assert_received 10000 end)
   end
 
   def callback(pid, payload), do: send(pid, payload)
