7ad5171f166c7054d617860752721825e647dfc9
Bernard Duggan <bernard@hippware.com>
2018-04-18 01:07:02 +0000

    Add config

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :procrastinator, key: :value
+#     config :dawdle, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:procrastinator, :key)
+#     Application.get_env(:dawdle, :key)
 #
 # You can also configure a 3rd-party app:
 #
---
+++ config/prod.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :dawdle,
+    backend: Dawdle.Backend.SQS
+
+config :dawdle, Dawdle.Backend.SQS,
+    region: "us-east-1",
+    queues: [], # Add your SQS queues here
+    workers_per_queue: 1
