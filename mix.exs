76403d79e9e256109a465d1a8e0f280412dc34dd
Phil Toland <phil.toland@gmail.com>
2019-04-15 16:57:44 -0600

    Update Dialyxir

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Dawdle.MixProject do
     [
       {:confex, "~> 3.4"},
       {:credo, "~> 1.0", only: [:dev], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
       {:eventually, "~> 1.0", only: :test},
       {:ex_aws, "~> 2.0"},
       {:ex_aws_sqs, "~> 2.0"},
