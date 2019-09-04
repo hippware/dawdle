50aaddbe43e610a8aa1475d49539ed16863fefa5
Phil Toland <phil.toland@gmail.com>
2019-09-04 16:50:17 -0600

    Formatting

--- config/config.exs
+++ config/config.exs
@@ -15,6 +15,7 @@ config :ex_aws,
 
 config :dawdle, Dawdle.Backend.SQS,
   region: "us-west-2",
-  queue_url: "https://sqs.us-west-2.amazonaws.com/XXXXXXXXXXXX/hippware-dawdle-message-test"
+  queue_url:
+    "https://sqs.us-west-2.amazonaws.com/XXXXXXXXXXXX/hippware-dawdle-message-test"
 
 config :logger, level: :info
