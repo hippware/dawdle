b2ea3192994bbfcd4dc4984ee18d46ee989c0167
Phil Toland <phil.toland@gmail.com>
2019-09-25 12:05:34 -0600

    Setup ElasticMQ for testing

--- config/config.exs
+++ config/config.exs
@@ -1,21 +1,28 @@
 use Mix.Config
 
-config :ex_aws,
-  access_key_id: [
-    {:system, "AWS_ACCESS_KEY_ID"},
-    {:awscli, "default", 30}
-  ],
-  secret_access_key: [
-    {:system, "AWS_SECRET_ACCESS_KEY"},
-    {:awscli, "default", 30}
-  ]
+# config :ex_aws,
+#   access_key_id: [
+#     {:system, "AWS_ACCESS_KEY_ID"},
+#     {:awscli, "default", 30}
+#   ],
+#   secret_access_key: [
+#     {:system, "AWS_SECRET_ACCESS_KEY"},
+#     {:awscli, "default", 30}
+#   ]
+
+config :ex_aws, :sqs,
+  access_key_id: "foo",
+  secret_access_key: "bar",
+  scheme: "http://",
+  host: "localhost",
+  port: 9324,
+  region: "elasticmq"
 
 # config :dawdle,
 #   backend: Dawdle.Backend.SQS
 
 config :dawdle, Dawdle.Backend.SQS,
-  region: "us-west-2",
-  queue_url:
-    "https://sqs.us-west-2.amazonaws.com/XXXXXXXXXXXX/hippware-dawdle-test"
+  region: "elasticmq",
+  queue_url: "http://localhost:9324/queue/hippware-dawdle-test"
 
 config :logger, level: :info
